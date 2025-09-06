19ed4e991008a221e313653c0d332b3013520666
KowshikNaga <kowshiknaga386@gmail.com>
2025-09-06 18:54:40 +0530

    Refactor AZ validation logic and improve subnet mapping in Lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -9,7 +9,6 @@ def main():
     parser.add_argument('--asg-name', required=True, help='Name of the Auto Scaling Group')
     parser.add_argument('--az-list', required=True, help='Comma-separated list of AZs')
     parser.add_argument('--action', required=True, choices=['attach', 'detach'], help='Action to perform')
-
     args = parser.parse_args()
 
     az_list = [az.strip() for az in args.az_list.split(',') if az.strip()]
@@ -18,7 +17,7 @@ def main():
         sys.exit(1)
 
     client = boto3.client('autoscaling')
-
+    
     # Get current ASG configuration
     response = client.describe_auto_scaling_groups(AutoScalingGroupNames=[args.asg_name])
     if not response['AutoScalingGroups']:
@@ -27,6 +26,18 @@ def main():
 
     asg = response['AutoScalingGroups'][0]
     current_azs = set(asg['AvailabilityZones'])
+    current_subnets = set(asg.get('VPCZoneIdentifier', '').split(','))
+
+    # Map subnets to AZs
+    ec2_client = boto3.client('ec2')
+    subnet_info = ec2_client.describe_subnets(SubnetIds=list(current_subnets))
+    subnet_az_map = {s['AvailabilityZone']: s['SubnetId'] for s in subnet_info['Subnets']}
+
+    # Validate AZs exist in ASG subnets
+    invalid_azs = [az for az in az_list if az not in subnet_az_map]
+    if invalid_azs:
+        print(f"Error: The following AZs do not exist in ASG subnets: {invalid_azs}")
+        sys.exit(1)
 
     if args.action == 'detach':
         new_azs = current_azs - set(az_list)
